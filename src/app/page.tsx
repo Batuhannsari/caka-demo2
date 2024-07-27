ede0be5014f190e2495f54ec628ccf96b3f84352
Batuhannsari <batuhan.sari@aselsis.com.tr>
2024-07-27 20:02:52 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import frame3 from "../../public/Frame3.png";
 import frame4 from "../../public/Frame4.png";
 import { HeroParallax } from "./ui/hero-parallax";
 
-export default function Home() {
+export const Home: React.FC<any> = (props) => {
 
   return (
     <>
