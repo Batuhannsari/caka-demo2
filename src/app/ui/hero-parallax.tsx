bdfbd06d45a3b5258c298f5398bf8a32dadced82
Batuhannsari <batuhan.sari@aselsis.com.tr>
2024-07-28 04:10:10 +0300

    .

--- src/app/ui/hero-parallax.tsx
+++ src/app/ui/hero-parallax.tsx
@@ -52,10 +52,9 @@ export const HeroParallax = ({
         springConfig
     );
     const translateY = useSpring(
-        useTransform(scrollYProgress, [0, 0.2], [-700, 0]),
+        useTransform(scrollYProgress, [0, 0.2], [-700, -50]),
         springConfig
     );
-
     return (
         <div
             ref={ref}
@@ -69,9 +68,9 @@ export const HeroParallax = ({
                     translateY,
                     opacity,
                 }}
-                className="flex-grow flex flex-col justify-end w-full"
+                className="h-full w-full flex-grow flex flex-col justify-end"
             >
-                <motion.div className="flex flex-wrap justify-center space-x-4 mb-16 w-full max-w-6xl mx-auto">
+                <motion.div className="flex flex-wrap justify-center space-x-16 mb-16 w-full">
                     {firstRow.map((product) => (
                         <ProductCard
                             product={product}
@@ -80,7 +79,7 @@ export const HeroParallax = ({
                         />
                     ))}
                 </motion.div>
-                <motion.div className="flex flex-wrap justify-center space-x-4 mb-16 w-full max-w-6xl mx-auto">
+                <motion.div className="flex flex-wrap justify-center space-x-16 mb-16 w-full">
                     {secondRow.map((product) => (
                         <ProductCard
                             product={product}
@@ -97,12 +96,14 @@ export const HeroParallax = ({
 export const Header = () => {
     const words = `Denizcilik sektöründeki uzmanlığımızla, Ocean&Co olarak siz değerli müşterilerimize güvenilir ve profesyonel hizmetler sunmaktan gurur duyuyoruz. Denizin engin dünyasında, ihtiyaçlarınıza uygun çözümler geliştirmek için buradayız.`;
     return (
-        <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
-            <Image width={500} height={200} src={logo} alt="ocean&co" />
-            <div className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-                <TextGenerateEffect duration={2} filter={false} words={words} />
+        <>
+            <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
+                <Image width={500} height={200} src={logo} alt="ocean&co" />
+                <div className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+                    <TextGenerateEffect duration={2} filter={false} words={words} />
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
@@ -126,13 +127,13 @@ export const ProductCard = ({
                 y: -20,
             }}
             key={product.title}
-            className="group/product relative flex-shrink-0 w-[35%] max-w-xs md:max-w-[30%] aspect-w-16 aspect-h-9"
+            className="group/product relative flex-shrink-0 w-[35%] max-w-xs md:max-w-[35%]"
         >
             <Link
                 href={product.link}
                 className="block group-hover/product:shadow-2xl"
             >
-                <div className="relative w-full h-0 pb-[56.25%]"> {/* 16:9 aspect ratio */}
+                <div className="relative w-full pb-[56.25%]"> {/* 16:9 aspect ratio */}
                     <Image
                         src={product.thumbnail}
                         layout="fill"
